3f41b2071f4d2a8c673efc2ee7c3e7bcadd5a7be
HamzaOubada2 <oubham220@gmail.com>
2026-07-01 18:12:24 +0100

    feat: add Swagger documentation and configure TypeORM with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,30 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+    imports:[
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'mysql',
+                host: config.get<string>('DB_HOST'),
+                port: config.get<number>('DB_PORT'),
+                username: config.get<string>('DB_USERNAME'),
+                password: config.get<string>('DB_PASSWORD'),
+                database: config.get<string>('DB_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        }),
+    ],
+
+    controllers:[],
+    providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,// ignore any redundant firlds not specified in the DTO
+    forbidNonWhitelisted: true, // An error is given if the user submits unnecessary fields
+    transform: true
+  }))
+
+
+  const config = new DocumentBuilder()
+    .setTitle('CenterOskar API Platform')
+    .setDescription('The Official Backend API documentation for Center Oskar Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger documentation available at: ${await app.getUrl()}/api/docs`);
 }
 bootstrap();
